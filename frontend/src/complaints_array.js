4bc6bb958328650802659a1f72b679e8ae79ac32
PRC2004 <parth.prc.95@gmail.com>
2025-01-30 12:09:46 +0530

    Added Complaints_array

---
+++ frontend/src/complaints_array.js
@@ -0,0 +1,135 @@
+Complaint_types = [
+  "Solid Waste Management",
+  "Drainage",
+  "Storm Water Drain",
+  "Roads and Traffic",
+  "Factories",
+  "License",
+  "Water Supply",
+  "Colony Officer",
+  "Pest Control",
+  "Buildings",
+  "Health",
+  "Gardens",
+  "Others",
+];
+
+Solid_Waste_Management_subcomplaints = [
+  "Garbage not lifted from house gully",
+  "Collection point not attended properly",
+  "Garbage not lifted from Municipal Market",
+  "Garbage lorry not reported for service",
+  "Garbage lorry not covered",
+  "Providing/Removing/Replacing dustbins",
+  "Removal of dead animals",
+  "Sweeping of road",
+  "Cleaning of P.S.C block channels",
+  "No Attendance at public toilets",
+  "Removal of Debris",
+  "Silt to be lifted on road",
+  "Non-Attendant of Nuisance Detector",
+  "Lifting of Tree-Cutting",
+  "Air Pollution",
+  "Removal of Debris",
+  "Burning of garbage",
+];
+
+Drainage_subcomplaints = [
+  "Drainage chokes and blockages",
+  "Overflowing drains or manholes",
+  "Odour (Foul smell) from drains",
+  "Replacment of missing/damaged manholes/inspection",
+  "Raising of manhole except in monsoon",
+  "Cleaning of septic tank",
+];
+
+Storm_water_drainage_subcomplaints = [
+  "Flooding during monsoon",
+  "Pre-monsoon works halted",
+  "Provinding damaged/missing gratings manhole covers over SWD's",
+  "Removal of silt from nalla/across culverts, etc",
+  "Repair to damaged open SWD",
+  "Cleaning of water entrance",
+  "Cleaning of open SWD",
+  "unauthorised Stalls and Huts on nallas",
+];
+
+Roads_and_Traffic_subcomplaints = [
+  "Manhole",
+  "Bad Patches",
+  "Dangerous Tree",
+  "Flooding spot/ Crosscut",
+  "Divider",
+  "Bad Pactches",
+  "Unauthorized digging of road",
+  "Street lightning",
+  "Signals",
+  "Speed Breakers",
+];
+
+Factories_subcomplaints = [
+  "Nuisance due to masala mills",
+  "Air Pollution",
+  "Pollution due to chemical effluents",
+  "Unauthorised factory, workshop, or garage",
+];
+
+License_subcomplaints = [
+  "Unauthorised storage of explosives",
+  "Trade without license",
+  "Unauthorized workshop or Garage",
+  "Storage and sale of plastic bags",
+];
+
+Water_supply_subcomplaints = [
+  "Leaks in water lines",
+  "Burst water main",
+  "Contaminated water supply",
+  "Shortage of water supply",
+  "Unauthorised tapping of water connection",
+  "Use of booster pump",
+  "Providing water by tankers",
+  "Water supply during non-supply hours",
+  "Leakage near meter",
+];
+
+Colony_officer = [
+  "Unauthorised construction in slum",
+  "Unauthorised repairs/renovation in slum",
+  "Delay in transfer case",
+  "Unauthorised extension/Construction",
+  "Unauthorised commerical activity",
+];
+
+Pest_Control_subcomplaints = [
+  "Mosquito nuisance",
+  "Rat nuisance",
+  "Fogging",
+  "Nuisance due to white ants",
+  "Nuisance due to cockraoaaches",
+  "Unauthorised/Uncovered water storage tanks",
+];
+
+Building_subcomplaints = [
+  "Unauthorised construction/Development",
+  "Permission for temporary monsoon shed",
+  "Regularisation of balcony encolurses",
+  "Heavy leakage from ceiling",
+];
+
+Health_subcomplaints = [
+  "Issue of Birth/Death Certificate",
+  "Unauthorised Food Selling/Preparation",
+  "Unauthorised flour Mills",
+];
+
+Garden_subcomplaints = [
+  "Permission for tree Trimming",
+  "Roadside Tree trimming",
+  "Fallen Tree in Public Premises",
+  "Fallen Tree in PVT Premises",
+  "Garden Cleaning",
+  "Garden Lights",
+  "Lifting Tree Cutting/ Cut Wood",
+  "Garden Repairs",
+];
